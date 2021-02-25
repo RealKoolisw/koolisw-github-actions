8f8591f1fd5271b18903f40fa3f8d6e95026b4fb
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-02-25 08:48:16 +0800

    Add files via upload

--- UpdateToken.py
+++ UpdateToken.py
@@ -10,8 +10,8 @@ gh_repo=os.getenv('GH_REPO')
 #账号信息生成
 accountkey=['client_id','client_secret','ms_token']
 #account存在？
-if os.getenv('ACCOUNT_ADD')!='':
-    account=json.loads(os.getenv('ACCOUNT_ADD'))
+if os.getenv('ACCOUNT')!='':
+    account=json.loads(os.getenv('ACCOUNT'))
 else:
     account={'client_id':[],'client_secret':[],'ms_token':[]}
 account_add=os.getenv('ACCOUNT_ADD').split(",")

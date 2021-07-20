3f0891519e54032d88ec65f2f5f3198f02b5d945
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-07-20 22:21:56 +0800

    Update main.py

--- euserv/main.py
+++ euserv/main.py
@@ -1,3 +1,4 @@
+import os
 import re
 import json
 import time
@@ -5,8 +6,8 @@ import requests
 from bs4 import BeautifulSoup
 
 # 多个账户请使用空格隔开
-USERNAME = ""  # 用户名或邮箱
-PASSWORD = ""  # 密码
+USERNAME = os.environ["USERNAME"]  # 用户名或邮箱
+PASSWORD = os.environ["PASSWORD"]  # 密码
 
 # Telegram Bot Push https://core.telegram.org/bots/api#authorizing-your-bot
 TG_BOT_TOKEN = ''  # 通过 @BotFather 申请获得，示例：1077xxx4424:AAFjv0FcqxxxxxxgEMGfi22B4yh15R5uw
